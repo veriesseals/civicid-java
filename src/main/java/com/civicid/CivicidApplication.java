2d5a35bbaf447b4b67c5a338a319911760809014
Veries Seals <veries@me.com>
2026-04-16 21:12:54 -0500

    Created Java/Maven/Spring Project

---
+++ src/main/java/com/civicid/CivicidApplication.java
@@ -0,0 +1,13 @@
+package com.civicid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/civicid/CivicidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
