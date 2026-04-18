cae11ecc775e5b397a455e3a01030086ce05059b
Veries Seals <veries@me.com>
2026-04-18 12:31:34 -0500

    Created logic for user repository

--- src/main/java/com/civicid/apps/accounts/UserRepository.java
+++ src/main/java/com/civicid/apps/accounts/UserRepository.java
@@ -1,5 +1,11 @@
 package com.civicid.apps.accounts;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional; 
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}    
     
-}
+
