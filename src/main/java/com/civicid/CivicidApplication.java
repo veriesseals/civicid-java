67be7c53d16412e3242710a98d505f26d17eb1ef
Veries Seals <veries@me.com>
2026-04-18 08:39:14 -0500

    Updated Class CivicIdApplication

--- src/main/java/com/civicid/CivicidApplication.java
+++ src/main/java/com/civicid/CivicidApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CivicidApplication {
+public class CivicIdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CivicidApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CivicIdApplication.class, args);
+    }
 }
