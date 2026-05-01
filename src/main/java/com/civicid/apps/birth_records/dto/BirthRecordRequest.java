f29aa5ab871820e276ac5960b1bd4ad56c241700
Veries Seals <veries@me.com>
2026-05-01 17:22:30 -0500

    Created and updated BirthRecordRequest.java

---
+++ src/main/java/com/civicid/apps/birth_records/dto/BirthRecordRequest.java
@@ -0,0 +1,117 @@
+package com.civicid.apps.birth_records.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import java.time.LocalDate;
+
+// BirthRecordRequest is the shape of data we ACCEPT when
+// a registrar files a new birth record.
+// -------------------------------------------------------
+public class BirthRecordRequest {
+
+    @NotBlank(message = "Certificate number is required")
+    private String certificateNumber;
+
+    @NotNull(message = "Date of birth is required")
+    @Past(message = "Date of birth must be in the past")
+    private LocalDate dateOfBirth;
+
+    @NotBlank(message = "City of birth is required")
+    private String cityOfBirth;
+
+    @NotBlank(message = "State of birth is required")
+    private String stateOfBirth;
+
+    @NotBlank(message = "Country of birth is required")
+    private String countryOfBirth;
+
+
+    private String motherFirstName;
+    private String motherLastName;
+    private String fatherFirstName;
+    private String fatherLastName;
+    private String birthFacility;
+
+    // --- Getters & Setters ---
+
+    public String getCertificateNumber() {
+        return certificateNumber;
+    }
+
+    public void setCertificateNumber(String certificateNumber) {
+        this.certificateNumber = certificateNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCityOfBirth() {
+        return cityOfBirth;
+    }
+
+    public void setCityOfBirth(String cityOfBirth) {
+        this.cityOfBirth = cityOfBirth;
+    }
+
+    public String getStateOfBirth() {
+        return stateOfBirth;
+    }
+
+    public void setStateOfBirth(String stateOfBirth) {
+        this.stateOfBirth = stateOfBirth;
+    }
+
+    public String getCountryOfBirth() {
+        return countryOfBirth;
+    }
+
+    public void setCountryOfBirth(String countryOfBirth) {
+        this.countryOfBirth = countryOfBirth;
+    }
+
+    public String getMotherFirstName() {
+        return motherFirstName;
+    }
+
+    public void setMotherFirstName(String motherFirstName) {
+        this.motherFirstName = motherFirstName;
+    }
+
+    public String getMotherLastName() {
+        return motherLastName;
+    }
+
+    public void setMotherLastName(String motherLastName) {
+        this.motherLastName = motherLastName;
+    }
+
+    public String getFatherFirstName() {
+        return fatherFirstName;
+    }
+
+    public void setFatherFirstName(String fatherFirstName) {
+        this.fatherFirstName = fatherFirstName;
+    }
+
+    public String getFatherLastName() {
+        return fatherLastName;
+    }
+
+    public void setFatherLastName(String fatherLastName) {
+        this.fatherLastName = fatherLastName;
+    }
+
+    public String getBirthFacility() {
+        return birthFacility;
+    }
+
+    public void setBirthFacility(String birthFacility) {
+        this.birthFacility = birthFacility;
+    }
+}
---
+++ src/main/java/com/civicid/apps/birth_records/dto/BirthRecordResponse.java
@@ -0,0 +1,4 @@
+package com.civicid.apps.birth_records.dto;
+
+public class BirthRecordResponse {
+}
